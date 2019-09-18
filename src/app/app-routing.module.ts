ff272a854a8504141be4db799e6ba3a4e020053a
Jacob Boyles <fixterjake@gmail.com>
2019-09-18 10:43:33 -0400

    Working on new structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './views/home/home.component';
+import { HomeComponent } from './views/public/home/home.component';
+import { AdminHomeComponent } from './views/admin/admin-home/admin-home.component';
 
 
 const routes: Routes = [
-  {path: "", component: HomeComponent}
+  {path: "",           component: HomeComponent},
+  {path: "Admin/Home", component: AdminHomeComponent}
 ];
 
 @NgModule({
